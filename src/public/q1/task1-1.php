4ecb179a1fd3f8bae45b7046615a193ac1c938a7
mikan19 <mellamo.rina.ty@gmail.com>
2023-05-03 22:39:35 +0900

    perf: クラス化しファイルを切り分けました

--- src/public/q1/task1-1.php
+++ src/public/q1/task1-1.php
@@ -1,21 +1,4 @@
 <?php
-// $dbUserName = "root";
-// $dbPassword = "password";
-// $pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
-
-// $sql = "SELECT * FROM incomes";
-// $statement = $pdo->prepare($sql);
-// $statement->execute();
-// $incomes = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// echo "<pre>";
-// var_dump($incomes);
-// echo "<pre>";
-
-
-
-
-
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 use App\Incomes;
